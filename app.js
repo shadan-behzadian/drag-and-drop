c90f0031c33efae6aea7f1984f6a60fdcdeafcec
shadan-behzadian <shadan.behzadian@gmail.com>
2021-05-06 13:57:44 +0200

    drag and drop and individual resizing possible

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const draggables = document.querySelectorAll(".draggables");
 const container = document.querySelector(".container");
 const resizers = document.querySelectorAll(".resizer");
+console.log(draggables);
+const growBtn = document.getElementById("growBtn");
+growBtn.addEventListener("click", growContainer);
 
 let isResizing = false;
 // el.addEventListener("mousedown", mousedown);
@@ -12,8 +15,9 @@ function dragElements() {
 
     function mousedown(e) {
       console.log(e.target);
-      let currentBlock = e.target;
-      currentBlock.classList.add("current");
+      let classNames = e.target.classList.value;
+      // let currentBlock = e.target;
+      // currentBlock.classList.add("current");
       window.addEventListener("mousemove", mousemove);
       window.addEventListener("mouseup", mouseup);
 
@@ -43,44 +47,91 @@ function dragElements() {
 
 resizing();
 function resizing() {
-  for (let draggable of draggables) {
-    for (let resizer of resizers) {
-      resizer.addEventListener("mousedown", mousedown);
+  for (let resizer of resizers) {
+    resizer.addEventListener("mousedown", mousedown);
 
-      let currentResizer;
+    let currentResizer;
 
-      function mousedown(e) {
-        currentResizer = e.target;
-        isResizing = true;
-        let prevX = e.clientX;
-        // let prevY = e.clientY;
-        window.addEventListener("mousemove", mousemove);
-        window.addEventListener("mouseup", mouseup);
+    function mousedown(e) {
+      currentResizer = e.target;
+      isResizing = true;
+      let prevX = e.clientX;
+      // let prevY = e.clientY;
+      window.addEventListener("mousemove", mousemove);
+      window.addEventListener("mouseup", mouseup);
 
-        function mousemove(e) {
-          console.log("moving");
+      function mousemove(e) {
+        console.log("moving");
 
-          const rect = draggable.getBoundingClientRect();
-          if (currentResizer.classList.contains("e")) {
-            draggable.style.width = rect.width - (prevX - e.clientX) + "px";
-            // el.style.height = rect.height - (prevY - e.clientY) + "px";
-          } else if (currentResizer.classList.contains("w")) {
-            draggable.style.width = rect.width + (prevX - e.clientX) + "px";
-            draggable.style.left = rect.left - (prevX - e.clientX) + "px";
-          }
-
-          prevX = e.clientX;
-          //   prevY = e.clientY;
+        console.log(currentResizer.parentNode);
+        const rect = currentResizer.parentNode.getBoundingClientRect();
+        if (currentResizer.classList.contains("e")) {
+          currentResizer.parentNode.style.width =
+            rect.width - (prevX - e.clientX) + "px";
+          // el.style.height = rect.height - (prevY - e.clientY) + "px";
+        } else if (currentResizer.classList.contains("w")) {
+          currentResizer.parentNode.style.width =
+            rect.width + (prevX - e.clientX) + "px";
+          currentResizer.parentNode.style.left =
+            rect.left - (prevX - e.clientX) + "px";
         }
 
-        function mouseup() {
-          window.removeEventListener("mousemove", mousemove);
-          window.removeEventListener("mouseup", mouseup);
-          isResizing = false;
-        }
+        prevX = e.clientX;
+        //   prevY = e.clientY;
+      }
+
+      function mouseup() {
+        window.removeEventListener("mousemove", mousemove);
+        window.removeEventListener("mouseup", mouseup);
+        isResizing = false;
       }
     }
   }
 }
 
-//create table
+const exportButton = document.getElementById("export");
+exportButton.addEventListener("click", exportDetail);
+
+function exportDetail(e) {
+  e.preventDefault();
+  let boxOne = document.querySelector(".item");
+  let boxTwo = document.querySelector(".itemTwo");
+  let boxThree = document.querySelector(".itemThree");
+  console.log(boxOne.getBoundingClientRect());
+  console.log(boxTwo.getBoundingClientRect());
+  console.log(boxThree.getBoundingClientRect());
+
+  console.log("hi");
+}
+
+// let boxOneBtn = document.getElementById("boxOneBtn");
+// let boxTwoBtn = document.getElementById("boxTwoBtn");
+// let boxThreeBtn = document.getElementById("boxThreeBtn");
+
+//creat little boxes
+// function createBox(boxNum) {
+//   let copyBox = document.createElement("div");
+//   let layOut = document.querySelector(".layOut");
+
+//   copyBox.classList.add("draggables");
+//   copyBox.classList.add(boxNum);
+//   layOut.appendChild(copyBox);
+//   let resizerEast = document.createElement("div");
+//   let resizerWest = document.createElement("div");
+//   resizerEast.classList.add("resizer");
+//   resizerEast.classList.add("e");
+//   resizerWest.classList.add("resizer");
+//   resizerWest.classList.add("w");
+//   copyBox.appendChild(resizerEast);
+//   copyBox.appendChild(resizerWest);
+
+//   console.log(copyBox);
+// }
+
+//grow container
+
+function growContainer() {
+  containerRect = container.getBoundingClientRect();
+  console.log(containerRect);
+  container.style.height = containerRect.height + 40 + "px";
+}
